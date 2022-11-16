9f8c3982ae2bc66f61f1ac661860cb2bfd595777
Mellownyn <sunnyshettigar@gmail.com>
2022-11-16 14:15:53 -0600

    Allow sketch blocks to resize when window resizes

--- dynamic-page-elements.js
+++ dynamic-page-elements.js
@@ -2,40 +2,66 @@
 
 // If this function can ingest a input from user (defaulting at 16 if no input then we can have a resolution scaler!)
 
-function initGridBlocks (resolutionFactor) {
+function initGridBlocks (resFactor) {
 
     // Create an object that points to the container div
     const sketchGrid = document.querySelector('div.page-content div.sketch-grid');
     
     // Isolate square dim of sketch area
-    const gridAreaTxt = window.getComputedStyle(sketchGrid).height;
-    const gridAreaNum = gridAreaTxt.slice(0,gridAreaTxt.length-2);
-    
-    // Check if square dim is cleanly divisible by resolutionFactor
-    // If not, increment res factor down until it is 0
+    const gridDim = window.getComputedStyle(sketchGrid).width;
+    const gridDimNum = gridDim.slice(0,gridDim.length-2);
 
-    while (gridAreaNum%resolutionFactor != 0) {
-        resolutionFactor --;
-    }
+    // Compute the dimension of each block
+    const blockDim = gridDimNum/resFactor;
 
-    console.log(`Using a resolution factor of ${resolutionFactor}`)
+    // May not need this while loop segement...
+    // Because remainders are stored, we get an even num
+    // of grid squares
 
-    // Set dimension of blocks for sketch area
-    const blockDim = gridAreaNum/resolutionFactor;
-    
-    // Create style value for block dim
+    // console.log(`Using a grid dim of ${gridDimNum}`);
+    // console.log(`Using a block dim of ${blockDim}`);
+
+    // Set dimension of blocks for sketch area (divide dim by resolution)
     const blockDimStyle = `${blockDim}px`;
 
-    // For resolution factor squared, construct that many block objects for sketch area
+    // Compute how many sketch divs must be used...
+    const blocksNeeded = resFactor ** 2;
 
-    for (let i = 0; i < resolutionFactor**2; i++) {
+    for (let i = 0; i < blocksNeeded; i++) {
         const gridBlock = document.createElement('div');
         gridBlock.style.height = blockDimStyle;
         gridBlock.style.width = blockDimStyle;
+        gridBlock.classList.add('sketch-block');
         sketchGrid.appendChild(gridBlock);
     }
 
     }
 
-// Per Assignment Requirement, Start with a Resolution of 16!
-initGridBlocks(16);
+// Res Factor Set by Some Other Event (static for now)
+const resFactor = 16;
+
+// Custom requirement always have a resolution of 1000 pixels...!
+initGridBlocks(resFactor);
+
+// generate callback function
+function changeDim(e) {
+     // Get all sketch blocks
+     const blocks = document.querySelectorAll('.sketch-block');
+
+     // Recompute what the block dimension should be
+     const sketchGrid = document.querySelector('div.page-content div.sketch-grid');
+     const gridDim = window.getComputedStyle(sketchGrid).width;
+     const gridDimNum = gridDim.slice(0,gridDim.length-2);
+ 
+     // Recalculate block dim
+     const newBlockDim = gridDimNum / resFactor; // resFactor is global variable
+     const newBlockDimStyle = `${newBlockDim}px`;
+
+     blocks.forEach(block => {
+        block.style.height = newBlockDimStyle;
+        block.style.width = newBlockDimStyle;
+     });
+}
+
+// Add event listener to the window...
+window.addEventListener('resize', changeDim);
